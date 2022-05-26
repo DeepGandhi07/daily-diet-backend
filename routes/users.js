405ca52465e4cf3ab655cb886d429e968deb88bb
lukaszmielczarekdev <lukaszmielczarekdev@gmail.com>
2022-05-26 22:36:35 +0200

    bugfix user model update - update function v7

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { signin, signup } from "../controllers/users.js";
 import { updateProfile } from "../controllers/users.js";
-import auth from "../middleware/auth.js";
+// import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/signin", signin);
 router.post("/signup", signup);
-router.patch("/:id", auth, updateProfile);
+router.patch("/:id", updateProfile);
 
 export default router;
