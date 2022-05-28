0c1bd72cd99c17ec12a44c49a9c953a667ceefe5
lukaszmielczarekdev <lukaszmielczarekdev@gmail.com>
2022-05-28 10:26:09 +0200

    bugfix user model update - update function v20

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,16 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import UserModel from "../models/userModel.js";
 
+export const getUsers = async (req, res) => {
+  try {
+    const users = await UserModel.find();
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
 export const signin = async (req, res) => {
   const { email, password } = req.body;
 
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { signin, signup, updateProfile } from "../controllers/users.js";
+import {
+  getUsers,
+  signin,
+  signup,
+  updateProfile,
+} from "../controllers/users.js";
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
+router.get("/", auth, getUsers);
 router.post("/signin", signin);
 router.post("/signup", signup);
-router.patch("/", auth, updateProfile);
+router.patch("/:id", auth, updateProfile);
 
 export default router;
