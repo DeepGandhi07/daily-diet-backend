53c03d7c4ec4dbfc64ac0312e78e9800289326dd
lukaszmielczarekdev <lukaszmielczarekdev@gmail.com>
2022-06-11 22:42:37 +0200

    user created items models changed

--- models/mealModel.js
+++ models/mealModel.js
@@ -2,15 +2,8 @@ import mongoose from "mongoose";
 
 const mealSchema = mongoose.Schema({
   name: String,
-  demand: { kcal: Number, protein: Number, carbs: Number, fat: Number },
   products: [Object],
   nutrients: { kcal: Number, protein: Number, carbs: Number, fat: Number },
-  demandCoverage: {
-    kcal: { bgcolor: String, completed: Number, label: String },
-    protein: { bgcolor: String, completed: Number, label: String },
-    carbs: { bgcolor: String, completed: Number, label: String },
-    fat: { bgcolor: String, completed: Number, label: String },
-  },
   creator: {
     type: String,
     default: "admin",
