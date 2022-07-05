c418b836760cef2e2b2e02d1cd9e5e248427be5e
lukaszmielczarekdev <lukaszmielczarekdev@gmail.com>
2022-07-05 16:44:59 +0200

    error throwing test v2

--- controllers/users.js
+++ controllers/users.js
@@ -184,7 +184,7 @@ export const updateUserData = async (req, res) => {
 
       if (newPasswordCopy !== confirmNewPasswordCopy)
         // return res.status(400).json({ message: "Passwords don't match" });
-        throw new Error({ message: "Passwords don't match" });
+        throw new Error("Passwords don't match");
 
       if (!mongoose.Types.ObjectId.isValid(req.userId))
         return res.status(404).send("User not found");
